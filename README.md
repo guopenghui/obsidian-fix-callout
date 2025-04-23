ef4397e7eeeb1ea5363012ee64401bba7883e248
guopenghui <guopeng_hui@126.com>
2025-04-23 21:05:38 +0800

    change indent to space

--- README.md
+++ README.md
@@ -1,31 +1,33 @@
 ## Fix Callout Error
+
 Currently, using the `<` symbol in a callout code block will disrupt the parsing of the entire note.
 
 This plugin is a simple solution with only [32 lines of code](https://github.com/guopenghui/obsidian-fix-callout/blob/master/main.ts).
 
 Core logic is here:
-```ts
+
+````ts
 function fixedHyperMD() {
-	// @ts-ignore
-	const hyperMode = CodeMirror.getMode({}, {name: "hypermd"})
-	return StreamLanguage.define({
-		...hyperMode,
-		token(stream: CodeMirror.StringStream, state: any) {
-			// There is a ```, oh I'm entering or exiting a codeblock
-			if(state.quote && stream.match(/^```/)) {
-				state.__iscode = !state.__iscode;
-				return null
+    // @ts-ignore
+    const hyperMode = CodeMirror.getMode({}, { name: "hypermd" });
+    return StreamLanguage.define({
+        ...hyperMode,
+        token(stream: CodeMirror.StringStream, state: any) {
+            // There is a ```, oh I'm entering or exiting a codeblock
+            if (state.quote && stream.match(/^```/)) {
+                state.__iscode = !state.__iscode;
+                return null;
             }
             // If I'm in a codeblock, ignore '<', so it's not parsed as HTML
-            if(state.quote && state.__iscode && stream.match(/^</)) {
-                return "bad-<"
+            if (state.quote && state.__iscode && stream.match(/^</)) {
+                return "bad-<";
             }
             // Obsidian built-in parser takes care of the rest
-			return hyperMode.token(stream, state);
-		}
-	} as any)
+            return hyperMode.token(stream, state);
+        },
+    } as any);
 }
-```
+````
 
 ## Effect
 
--- main.ts
+++ main.ts
@@ -1,31 +1,31 @@
-import { StreamLanguage } from '@codemirror/language';
-import {Prec} from "@codemirror/state"
-import {Plugin} from 'obsidian';
+import { StreamLanguage } from "@codemirror/language";
+import { Prec } from "@codemirror/state";
+import { Plugin } from "obsidian";
 
 export default class FixCallout extends Plugin {
-	async onload() {
-		this.registerEditorExtension(Prec.high(fixedHyperMD()))
-	}
-	onunload() {}
+    async onload() {
+        this.registerEditorExtension(Prec.high(fixedHyperMD()));
+    }
+    onunload() {}
 }
 
 function fixedHyperMD() {
-	// @ts-ignore
-	const hyperMode = CodeMirror.getMode({}, {name: "hypermd"})
-	return StreamLanguage.define({
-		...hyperMode,
-		token(stream: CodeMirror.StringStream, state: any) {
-			// There is a ```, oh I'm entering or exiting a codeblock
-			if(state.quote && stream.match(/^```/)) {
-				state.__iscode = !state.__iscode;
-				return null
+    // @ts-ignore
+    const hyperMode = CodeMirror.getMode({}, { name: "hypermd" });
+    return StreamLanguage.define({
+        ...hyperMode,
+        token(stream: CodeMirror.StringStream, state: any) {
+            // There is a ```, oh I'm entering or exiting a codeblock
+            if (state.quote && stream.match(/^```/)) {
+                state.__iscode = !state.__iscode;
+                return null;
             }
             // If I'm in a codeblock, ignore '<', so it's not parsed as HTML
-            if(state.quote && state.__iscode && stream.match(/^</)) {
-                return "bad-<"
+            if (state.quote && state.__iscode && stream.match(/^</)) {
+                return "bad-<";
             }
             // Obsidian built-in parser takes care of the rest
-			return hyperMode.token(stream, state);
-		}
-	} as any)
+            return hyperMode.token(stream, state);
+        },
+    } as any);
 }
